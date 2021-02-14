71dddc018ff791b3e39e1a72fe2450b7fcc0a2e9
meili <meiliham1@gmail.com>
2021-02-14 13:13:43 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
 # Add  code here!
+def prime?(num)
+    (1..(num - 1)).each do |n|
+         if num % n == 0
+           true
+         else num %n != 0
+            false
+         end
+      end
+    end
